e911b9ea0fd5e2962b638ca4cf780dc0c7bfc32a
Evaloper <gbajeeva@yahoo.com>
2023-03-18 14:39:15 +0100

    Basic Js

---
+++ operator.js
@@ -0,0 +1,30 @@
+let num1 = 10;
+let num2 = 5;
+
+
+console.log(num1 + num2 );
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 %  num2);
+
+console.log(num1++);
+console.log(num2--);
+
+let input1 = prompt("Type Input1");
+let input2 = prompt("Type Input2");
+
+parseFloat(input1);
+parseFloat(input2);
+
+let sum = input1 + input2;
+let substract = input1 - input2;
+let multiply = input1 * input2;
+let divide = input1 / input2;
+let remainder = input1 % input2;
+
+alert(sum);
+alert(substract);
+alert(multiply);
+alert(divide);
+alert(remainder);
